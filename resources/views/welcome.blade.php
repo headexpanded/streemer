92e11718bf6c35449b65351058ba10200f73dcc9
MarkH <markh@bellemoncon>
2023-02-14 16:30:25 +0100

    re-styling home page (incomplete)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
 
         <title>Streemer</title>
 
@@ -20,6 +21,7 @@
             }
         </style>
     </head>
+    @extends('custom')
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
